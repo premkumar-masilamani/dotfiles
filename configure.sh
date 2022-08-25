06cc63d7e8feff1802b97eac597a95f4b19f0325
Premkumar Masilamani <smileprem@users.noreply.github.com>
2022-08-24 17:44:06 -0700

    Added protodot config file

--- configure.sh
+++ configure.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Configure Z Shell
-ln -sf /Users/smileprem/Code/personal/dotfiles/zshrc ~/.zshrc
+ln -sf /Users/smileprem/Code/personal/dotfiles/zsh/zshrc.profile ~/.zshrc
 
 # Configure Alacritty
-ln -sf /Users/smileprem/Code/personal/dotfiles/alacritty.yml ~/.alacritty.yml
+ln -sf /Users/smileprem/Code/personal/dotfiles/alacritty/alacritty.yml ~/.alacritty.yml
+
+# Configure Prododot
+ln -sf /Users/smileprem/Code/personal/dotfiles/protodot/config.json ~/config.json
